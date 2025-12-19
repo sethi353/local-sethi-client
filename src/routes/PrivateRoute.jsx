80646b58b1bf2df32a6ade8624be901eb1264681
sethi353 <sethisaha56@gmail.com>
2025-12-19 16:23:47 +0600

    Updated PrivateRoute.jsx file

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,17 +1,4 @@
-// import { useContext } from "react";
-// import { Navigate } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContext";
 
-// const PrivateRoute = ({ children }) => {
-//   const { user, loading } = useContext(AuthContext);
-
-//   if (loading) return <p className="text-center mt-20">Loading...</p>;
-//   if (!user) return <Navigate to="/login" replace />;
-
-//   return children;
-// };
-
-// export default PrivateRoute;
 
 import { Navigate, useLocation } from "react-router-dom";
 import { useContext } from "react";
